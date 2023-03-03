a78b2808263163338f81cbb96cbf1dde9decb4cb
Lucas Dellandréa <lucas.dellandrea@conectra.com.br>
2023-03-03 16:27:45 -0300

    aula 8

---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu Nome é: {{nome}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            nome: 'Ainda não sei'
+        }
+    },
+    created() {
+        setTimeout(() => {
+        this.nome = 'Lucas'
+        }, 1000);
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = 'Antônio'
+        }, 2000);
+    }
+}
+</script>
