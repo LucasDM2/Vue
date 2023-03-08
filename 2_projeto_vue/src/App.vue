ded8bd6f1d9db8a7566ff4b39657db652b8cc786
Lucas Dellandréa <lucas.dellandrea@conectra.com.br>
2023-03-08 17:08:25 -0300

    adiciona componentes inputtext e submit

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -3,18 +3,21 @@
     <PrimeiroComponente />
     <p>O dado de app é: {{testando}}</p>
     <LifeCycle />
+    <pessoa />
   </div>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrmeiroComponente.vue'
   import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
 
   export default {
     name: 'App',
     components: {
       PrimeiroComponente,
-      LifeCycle
+      LifeCycle,
+      Pessoa
     },
     data() {
       return{
---
+++ 2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Estou trabalhando no moomento.</p>
+        <p>Uso as seguintes tecnologias:</p>
+    <ul>
+        <li>JavaScript</li>
+        <li>PHP</li>
+        <li>Python</li>
+    </ul>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Info'
+    }
+</script>
